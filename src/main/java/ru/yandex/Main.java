e3abd7e5ee0a28392cb28b3a11bb3e5fd66317a2
Sofia Kuboreva <skuboreva@gmail.com>
2026-05-14 11:46:46 +0200

    Add main menu command handling (#1)

--- src/main/java/ru/yandex/Main.java
+++ src/main/java/ru/yandex/Main.java
@@ -1,6 +1,39 @@
 package ru.yandex;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printMenu();
+
+            int command = scanner.nextInt();
+            switch (command) {
+                case 1:
+                    System.out.println("Выполняется команда 1");
+                    break;
+                case 2:
+                    System.out.println("Выполняется команда 2");
+                    break;
+                case 3:
+                    System.out.println("Выполняется команда 3");
+                    break;
+                case 4:
+                    System.out.println("Программа завершена.");
+                    return;
+                default:
+                    System.out.println("Извините, такой команды пока нет.");
+            }
+        }
+    }
+
+    static void printMenu() {
+        System.out.println("Что вы хотите сделать? ");
+        System.out.println("1 - Ввести количество шагов за определённый день");
+        System.out.println("2 - Изменить цель по количеству шагов в день");
+        System.out.println("3 - Напечатать статистику за определённый месяц");
+        System.out.println("4 - Выйти из приложения");
     }
 }
