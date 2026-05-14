7cb862e8d7408777fcf48634173a5abae7e32599
SofiaKubo <skuboreva@gmail.com>
2026-05-14 15:34:18 +0200

    Add step conversion helpers

--- src/main/java/ru/yandex/Converter.java
+++ src/main/java/ru/yandex/Converter.java
@@ -1,4 +1,16 @@
 package ru.yandex;
 
 public class Converter {
+    private static final int STEP_LENGTH_CM = 75;
+    private static final int CM_PER_KM = 100000;
+    private static final int CALORIES_PER_STEP = 50;
+    private static final int CALORIES_PER_KILOCALORIE = 1000;
+
+    int convertToKm(int steps) {
+        return steps * STEP_LENGTH_CM / CM_PER_KM;
+    }
+
+    int convertStepsToKilocalories(int steps) {
+        return steps * CALORIES_PER_STEP / CALORIES_PER_KILOCALORIE;
+    }
 }
